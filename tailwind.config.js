b29ee8091039c1145c1025f46be18698f069a18a
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-28 02:43:31 +0000

    configure tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['src/*.js', 'src/components/.js', 'src/*.jsx', 'src/components/*.jsx', 'src/components/**/.jsx', 'src/components/**/.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#',
+        lightGray: '#D9D9D9'
+      },
+      screens: {
+        'sm': '430px',
+        'md': '720px',
+        'lg': '960px',
+        'xlg': '1280px'
+      }
+    },
   },
   plugins: [],
 }
