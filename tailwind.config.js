6a956f60eb1d9bb548b65b6eaaf44799f3273b27
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-28 02:27:04 +0000

    setup tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['src/*.js', 'src/components/.js', 'src/*.jsx', 'src/components/*.jsx', 'src/components/**/.jsx', 'src/components/**/.js'],
   theme: {
     extend: {},
   },
