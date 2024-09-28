5cd1343c006ff61c96f2e69d87c0f3acb65aafea
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-28 02:44:44 +0000

    configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: '#',
-        lightGray: '#D9D9D9'
+        'black': '#000',
+        'brown': '#7E0808',
+        'lightblue': '#2DAFAF',
+        'lightgray': '#4A4A4A',
+        'purple': '#9747FF'
       },
       screens: {
-        'sm': '430px',
-        'md': '720px',
+        'sm': '480px',
+        'md': '620px',
         'lg': '960px',
-        'xlg': '1280px'
-      }
+        'xl': '1280px'
+      },
     },
   },
   plugins: [],
