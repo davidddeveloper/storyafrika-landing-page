78a1d74ad15963f42e1e2cbe2fa00b355dd6aa6a
davidddeveloper <programmingdonebydavid@gmail.com>
2024-10-24 16:32:29 +0000

    remove know us image and replaced it with just 'Know us?'

--- src/components/KnowUsLink.jsx
+++ src/components/KnowUsLink.jsx
@@ -2,12 +2,8 @@ import { Link } from "react-router-dom"
 import Know_us  from "../assets/svgs/Know us_.svg"
 
 export default function KnowUsLink ({color}) {
-    const classes = `hidden md:block bg-${color} hover:text-lightblue fixed bottom-10 right-[10%] h-[22px] z-40 w-[82px] cursor-pointer`
+    const classes = `hidden md:block text-${color} hover:text-lightblue fixed bottom-10 right-[10%] h-[22px] z-40 w-[82px] cursor-pointer`
     return (
-        <Link to="/about-us" smooth={true} duration={500}> <img
-        className={classes}
-        src={Know_us}
-        alt="Writers SVG"
-        /></Link>
+        <Link to="/about-us" smooth={true} duration={500} className={classes}> Know us?</Link>
     )
 }
